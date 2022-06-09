48c06484a020eed4258da89f7967c81060684716
cursey <cursey@live.com>
2022-06-09 09:48:41 -0700

    API: Add mem.scan_all, mem.scan_str, mem.scan_all_str

--- src/api.rs
+++ src/api.rs
@@ -35,6 +35,36 @@ pub fn register(ctx: &Context) -> Result<()> {
     )?;
 
     mem.set(
+        "scan_all",
+        ctx.create_function(|_ctx, (start, len, pat): (usize, usize, String)| {
+            match scan_all(span(start, len), pat.as_str()) {
+                Ok(m) => Ok(m),
+                Err(e) => Err(rlua::Error::external(e)),
+            }
+        })?,
+    )?;
+
+    mem.set(
+        "scan_str",
+        ctx.create_function(|_ctx, (start, len, pat): (usize, usize, String)| {
+            match scan_str(span(start, len), pat.as_str()) {
+                Ok(m) => Ok(m),
+                Err(e) => Err(rlua::Error::external(e)),
+            }
+        })?,
+    )?;
+
+    mem.set(
+        "scan_all_str",
+        ctx.create_function(|_ctx, (start, len, pat): (usize, usize, String)| {
+            match scan_all_str(span(start, len), pat.as_str()) {
+                Ok(m) => Ok(m),
+                Err(e) => Err(rlua::Error::external(e)),
+            }
+        })?,
+    )?;
+
+    mem.set(
         "test",
         ctx.create_function(|_ctx, (a, b): (i32, i32)| Ok(a + b))?,
     )?;
@@ -124,6 +154,66 @@ mod test {
             assert_eq!(
                 ctx.load(script.as_str()).eval::<Option<usize>>().unwrap(),
                 Some(data.as_ptr() as usize + 10)
+            );
+        });
+    }
+
+    #[test]
+    fn mem_scan_all() {
+        let lua = Lua::new();
+        lua.context(|ctx| {
+            register(&ctx).unwrap();
+
+            let data: &[u8] = &[0, 1, 2, 3, 42, 77, 0, 1, 2, 3, 5, 6, 7];
+            let script = format!(
+                r#"return mem.scan_all(0x{:x}, {}, "00 ? ? 03")"#,
+                data.as_ptr() as usize,
+                data.len()
+            );
+
+            assert_eq!(
+                ctx.load(script.as_str()).eval::<Vec<usize>>().unwrap(),
+                vec![data.as_ptr() as usize + 0, data.as_ptr() as usize + 6]
+            );
+        });
+    }
+
+    #[test]
+    fn mem_scan_str() {
+        let lua = Lua::new();
+        lua.context(|ctx| {
+            register(&ctx).unwrap();
+
+            let data = "Hello, world!";
+            let script = format!(
+                r#"return mem.scan_str(0x{:x}, {}, "world")"#,
+                data.as_ptr() as usize,
+                data.len()
+            );
+
+            assert_eq!(
+                ctx.load(script.as_str()).eval::<Option<usize>>().unwrap(),
+                Some(data.as_ptr() as usize + 7)
+            );
+        });
+    }
+
+    #[test]
+    fn mem_scan_all_str() {
+        let lua = Lua::new();
+        lua.context(|ctx| {
+            register(&ctx).unwrap();
+
+            let data = "Hello, world! Hello, moon!";
+            let script = format!(
+                r#"return mem.scan_all_str(0x{:x}, {}, "Hello")"#,
+                data.as_ptr() as usize,
+                data.len()
+            );
+
+            assert_eq!(
+                ctx.load(script.as_str()).eval::<Vec<usize>>().unwrap(),
+                vec![data.as_ptr() as usize + 0, data.as_ptr() as usize + 14]
             );
         });
     }
