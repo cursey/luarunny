79d2a15e810570ce0b5fa8f18487df14d6cfdf05
cursey <cursey@live.com>
2022-08-28 15:45:14 -0700

    Lib: Add basic Lua REPL

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,15 @@
 mod api;
 mod mem;
 
-use rlua::Lua;
-use std::{env::current_dir, ffi::c_void, path::PathBuf, thread};
+use rlua::{InitFlags, Lua, MultiValue, StdLib};
+use std::{ffi::c_void, path::PathBuf, sync::Mutex, thread};
 use windows::Win32::{
     Foundation::{BOOL, HINSTANCE},
     System::{LibraryLoader::GetModuleFileNameA, SystemServices::DLL_PROCESS_ATTACH},
 };
+
+#[macro_use]
+extern crate lazy_static;
 
 #[no_mangle]
 extern "system" fn DllMain(module: HINSTANCE, reason: u32, _reserved: *const c_void) -> BOOL {
@@ -30,10 +33,14 @@ fn start_thread(module: HINSTANCE) {
     let options = eframe::NativeOptions::default();
 
     eframe::run_native(
-        "My egui App",
+        "LuaRunny",
         options,
         Box::new(|_cc| Box::new(MyApp::new(filepath))),
     )
+}
+
+lazy_static! {
+    static ref MSG: Mutex<String> = Mutex::new(String::new());
 }
 
 struct MyApp {
@@ -41,19 +48,39 @@ struct MyApp {
     name: String,
     age: u32,
     lua: Lua,
+    input: String,
 }
 
 impl MyApp {
     pub fn new(filepath: PathBuf) -> Self {
-        let lua = Lua::new();
+        let lua = unsafe {
+            Lua::unsafe_new_with_flags(
+                StdLib::ALL_NO_DEBUG,
+                InitFlags::PCALL_WRAPPERS | InitFlags::LOAD_WRAPPERS,
+            )
+        };
+
         lua.context(|ctx| {
             api::register(&ctx).expect("Failed to register the API with the Lua context");
+
+            ctx.globals()
+                .set(
+                    "print",
+                    ctx.create_function(|_ctx, str: String| {
+                        MSG.lock().unwrap().push_str(&str);
+                        Ok(())
+                    })
+                    .expect("Failed to create the print function"),
+                )
+                .expect("Failed to register the print function");
         });
+
         Self {
             filepath,
             name: "cursey".to_string(),
             age: 30,
             lua,
+            input: "".to_string(),
         }
     }
 }
@@ -61,25 +88,48 @@ impl MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Applicaiton");
-            ui.horizontal(|ui| {
-                ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
-            ui.label(format!(
-                "curdir: '{}'",
-                current_dir().unwrap().to_str().unwrap()
-            ));
-            ui.label(format!("modpath: '{}'", self.filepath.to_str().unwrap()));
-            if ui.button("run script").clicked() {
-                self.lua.context(|ctx| {
-                    assert_eq!(ctx.load("mem.test(2, 3)").eval::<i32>().unwrap(), 5);
-                });
+            egui::TextEdit::multiline(&mut *MSG.lock().unwrap())
+                .interactive(false)
+                .cursor_at_end(true)
+                .desired_width(f32::INFINITY)
+                .desired_rows(25)
+                .font(egui::TextStyle::Monospace)
+                .show(ui);
+
+            let output = egui::TextEdit::singleline(&mut self.input)
+                .interactive(true)
+                .desired_width(f32::INFINITY)
+                .font(egui::TextStyle::Monospace)
+                .lock_focus(true)
+                .show(ui);
+
+            if output.response.lost_focus() && ui.input().key_pressed(egui::Key::Enter) {
+                // refocus the input field
+                output.response.request_focus();
+
+                MSG.lock()
+                    .unwrap()
+                    .push_str(format!("> {}\n", self.input).as_str());
+                self.lua
+                    .context(|ctx| match ctx.load(&self.input).eval::<MultiValue>() {
+                        Ok(values) => {
+                            MSG.lock().unwrap().push_str(
+                                format!(
+                                    "{}\n",
+                                    values
+                                        .iter()
+                                        .map(|value| format!("{:?}", value))
+                                        .collect::<Vec<_>>()
+                                        .join("\t")
+                                )
+                                .as_str(),
+                            );
+                        }
+                        Err(e) => {
+                            MSG.lock().unwrap().push_str(format!("{}\n", e).as_str());
+                        }
+                    });
+                self.input.clear();
             }
         });
     }
